ad84b78d1801a15ad2991344d87d542338664562
Yuji Teshima <yuji.teshima@gmail.com>
2022-10-12 23:04:53 +0900

    ac

---
+++ syouzin/abc152b.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  // aをb回
+  string ab = "";
+  string sa = to_string(a);
+  rep(i, b) { ab += sa; }
+  string ba = "";
+  string sb = to_string(b);
+  rep(i, a) { ba += sb; }
+  if (ab >= ba) {
+    cout << ba << endl;
+  } else {
+    cout << ab << endl;
+  }
+  return 0;
+}
