e18d44805607a2f23317877d125441f5755e0018
Yuji Teshima <yuji.teshima@gmail.com>
2022-10-17 23:52:02 +0900

    ac

---
+++ syouzin/agc030a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a + b + 1 >= c) {
+    cout << b + c << endl;
+  } else {
+    cout << b + a + b + 1 << endl;
+  }
+  return 0;
+}
