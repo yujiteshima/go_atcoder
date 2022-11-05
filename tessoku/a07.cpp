e3d47a5f59c87b7e9a28c9d7a2b5dcf913cfd588
Yuji Teshima <yuji.teshima@gmail.com>
2022-11-05 17:29:07 +0900

    ac

---
+++ tessoku/a07.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  int d, n;
+  cin >> d >> n;
+  vector<int> a(d + 1, 0);
+
+  repp(i, 0, n) {
+    int l, r;
+    cin >> l >> r;
+    ++a[l];
+    --a[r + 1];
+  }
+  // for (auto ai : a) {
+  //   cout << ai << " ";
+  // }
+  int sankasya = 0;
+  repp(i, 1, d + 1) {
+    sankasya += a[i];
+    cout << sankasya << endl;
+  }
+  return 0;
+}
