a925d769683113b5328a3f74fd3da3b9ff48ad9c
Yuji Teshima <yuji.teshima@gmail.com>
2022-11-06 22:19:38 +0900

    ac

---
+++ tessoku/b04.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int btoi(const string& bin) { return strtoul(bin.c_str(), NULL, 2); }
+
+int main() {
+  string binary;
+  cin >> binary;
+  int ans = btoi(binary);
+  cout << ans << endl;
+  return 0;
+}
