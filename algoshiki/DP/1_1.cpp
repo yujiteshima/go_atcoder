97ef71320191e0400fbc05a1179c8028b1730631
Yuji Teshima <yuji.teshima@gmail.com>
2022-11-10 23:29:55 +0900

    ac

---
+++ algoshiki/DP/1_1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  int n, x, y;
+  cin >> n >> x >> y;
+  vector<int> dp(n);
+  dp[0] = x;
+  dp[1] = y;
+  repp(i, 2, n) { dp[i] = (dp[i - 2] + dp[i - 1]) % 100; }
+  cout << dp[n - 1] << endl;
+  return 0;
+}
