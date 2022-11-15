fc00a5102d86db09e86f83a4b9812e309852e8fe
Yuji Teshima <yuji.teshima@gmail.com>
2022-11-15 22:57:11 +0900

    ac

---
+++ algoshiki/glaph/tree/q1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+  vector<int> A(n);
+  repp(i, 1, n) { cin >> A[i]; }
+
+  int res = 0;
+  while (x != 0) {
+    ++res;
+    x = A[x];
+  }
+  cout << res << endl;
+  return 0;
+}
