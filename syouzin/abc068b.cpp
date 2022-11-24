df5cd06caac6618ba76f2ece3d711b9c14d722ef
Yuji Teshima <yuji.teshima@gmail.com>
2022-11-24 23:18:46 +0900

    ac

---
+++ syouzin/abc068b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int calc_sum_breaknum(int n) {
+  int sum = 0;
+  while (n <= 2) {
+    n /= 2;
+    sum++;
+  }
+  return sum;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int ans;
+  cout << calc_sum_breaknum(n) << endl;
+  return 0;
+}
