e4b113029a89a7870259df17604fdfdc0347b657
Yuji Teshima <yuji.teshima@gmail.com>
2022-12-11 21:18:54 +0900

    ac

--- algoshiki/DP/1_2.cpp
+++ algoshiki/DP/1_2.cpp
@@ -22,9 +22,12 @@ T calc_dist(T x1, T y1, T x2, T y2) {
 int main() {
   int n;
   cin >> n;
-  vector<int> a(n);
-  for (auto& ai : a) cin >> ai;
-  vector<int> dp(n);
-
+  vector<int> a(n + 10);
+  rep(i, n) cin >> a[i];
+  vector<int> dp(n + 10);
+  dp[0] = 0;
+  dp[1] = a[1];
+  repp(i, 2, n) { dp[i] = min(dp[i - 1] + a[i], dp[i - 2] + a[i] * 2); }
+  cout << dp[n - 1] << endl;
   return 0;
 }
