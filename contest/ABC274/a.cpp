d03f88b1dc23049d6adbefa983cc8de2f75e8bf8
Yuji Teshima <yuji.teshima@gmail.com>
2023-01-31 23:26:43 +0900

    ac

---
+++ contest/ABC274/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007;  // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template <class T>
+void chmin(T& a, T b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T& a, T b) {
+  if (a < b) a = b;
+}
+template <class T>
+T calc_dist(T x1, T y1, T x2, T y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+  double ansdouble = (b / a) + 0.0005;
+  string ansstring = to_string(ansdouble);
+  ansstring = ansstring.substr(0, 5);
+  cout << ansstring << endl;
+
+  return 0;
+}
